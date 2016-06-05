8c7bf34f2e1e51ae621dcf75592e23731429de6e
Dmytro Pavlenko <atosdo@live.com>
2016-06-05 15:09:57 +0300

    Add WebApi controller with some functionality

--- SimpleWebApp.CMS/Controllers/ArticleController.cs
+++ SimpleWebApp.CMS/Controllers/ArticleController.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
+using SimpleWebApp.BusinessLogic;
+using SimpleWebApp.BusinessLogic.Abstract;
+using SimpleWebApp.BusinessLogic.DTO;
+using SimpleWebApp.DAL.EF;
 
 namespace SimpleWebApp.CMS.Controllers
 {
     public class ArticleController : ApiController
     {
+        private readonly IArticleService _articleService = new ArticleService(
+            new ArticlesRepository(new EfDbContext()), new MyMapper());
 
+        //public ArticleController(IArticleService articleService)
+        //{
+        //    _articleService = articleService;
+        //}
+
+        public ArticleEditDto Save(ArticleEditDto dto)
+        {
+            _articleService.Save(dto);
+
+            return dto;
+        }
     }
 }
